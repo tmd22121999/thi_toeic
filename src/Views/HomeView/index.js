d11e4a7c59523f9e0b364c0f339641390c083eca
tmd22121999 <tmdtmd22121999@gmail.com>
2022-10-26 09:25:23 +0700

    first commit

---
+++ src/Views/HomeView/index.js
@@ -0,0 +1,16 @@
+import './styles.css';
+// import { Grid, Card, Text } from "@nextui-org/react";
+
+
+function HomeView() {
+  return (
+    <div className="Home">    
+      <div>
+        <h1 className='HeadingText'>{'Heading'}</h1>
+        <p className='DescriptionText'>{'description'}</p>
+      </div>
+    </div>
+  );
+}
+
+export default HomeView;
